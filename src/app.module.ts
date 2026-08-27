470ef9969234408244b46ac33ca10fe9690f130a
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-08-26 21:23:28 -0300

    feat: add models resource in src folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
+import { ModelsModule } from './models/models.module';
+import { VersionsModule } from './versions/versions.module';
 
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, ModelsModule, VersionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
