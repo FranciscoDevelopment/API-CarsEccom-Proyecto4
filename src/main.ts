7e41334a355abe02b43dd403efa6422e2548c188
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-08-28 11:44:34 -0300

    feat: configure the global prefix and enable versioning in the application.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+//import ResponseInterceptor ;
 
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix( "api" ) ; 
+
+  app.enableVersioning( {type: VersioningType.URI, defaultVersion: '1'} ) ;
+
+  app.useGlobalPipes(
+    new ValidationPipe(
+      {
+        whitelist: true ,
+        forbidNonWhitelisted: true ,
+        transform: true
+      }
+    )
+  );
+
+  //app.useGlobalInterceptors( new ResponseInterceptor() )
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
