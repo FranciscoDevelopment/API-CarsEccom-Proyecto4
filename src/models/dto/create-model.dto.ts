e2cfdc15e89c51f0648ce49591cff86dc66dfe9d
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-08-28 20:09:27 -0300

    feat: implement CreateModelDto with validation rules

--- src/models/dto/create-model.dto.ts
+++ src/models/dto/create-model.dto.ts
@@ -1,1 +1,13 @@
-export class CreateModelDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from "class-validator";
+
+export class CreateModelDto {
+
+    @IsNotEmpty( {message: "The brand is required"} )
+    @IsString( {message: "The value is not valid"} )
+    brand! : string
+
+    @IsNotEmpty( {message: "The model name is required"} )
+    @IsString( {message: "The value is not valid"} )
+    name! : string
+
+}
