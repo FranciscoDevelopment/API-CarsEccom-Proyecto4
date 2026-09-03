641c1bb07489245926f7a574c8f4385b3d1a2b90
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-09-03 14:57:37 -0300

    feat: implement create, findAll, and findOne methods in VersionsService

--- src/versions/versions.service.ts
+++ src/versions/versions.service.ts
@@ -1,26 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVersionDto } from './dto/create-version.dto';
 import { UpdateVersionDto } from './dto/update-version.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class VersionsService {
+
+  constructor( private readonly prismaORM : PrismaService ) {}
+
+
   create(createVersionDto: CreateVersionDto) {
-    return 'This action adds a new version';
+    
+    return this.prismaORM.versions.create(
+      {
+        data: createVersionDto,
+        select: {name: true, model_name: true, brand: true}
+      }
+    )
+
   }
 
-  findAll() {
-    return `This action returns all versions`;
+
+  async findAll() {
+  
+    return await this.prismaORM.versions.findMany() ;
+  
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} version`;
+
+  async findOne(id: number) {
+  
+    return await this.prismaORM.versions.findUnique(
+      {
+        where: {id}
+      }
+    ) 
+  
   }
+
 
   update(id: number, updateVersionDto: UpdateVersionDto) {
     return `This action updates a #${id} version`;
   }
 
+
   remove(id: number) {
     return `This action removes a #${id} version`;
   }
+
 }
