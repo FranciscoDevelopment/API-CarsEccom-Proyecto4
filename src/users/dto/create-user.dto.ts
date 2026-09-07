ddb078f7c03de81705c2a67ab4734a5176467300
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-09-07 11:20:03 -0300

    fix: more details in dto fields validators

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 // src/users/dto/create-user.dto.ts
-import { IsEmail, IsString, MinLength, MaxLength, Matches, IsStrongPassword } from 'class-validator';
+import { IsEmail, IsString, MinLength, MaxLength, Matches, IsStrongPassword, maxLength } from 'class-validator';
 import { passwordRegex } from 'src/common/some-regex';
 
 export class CreateUserDto {
@@ -8,12 +8,13 @@ export class CreateUserDto {
 
   @IsString()
   @MinLength(8, { message: 'The password must be at least 8 characters long' })
+  @MaxLength( 72, {message: 'The password is too long'} )
   @Matches( passwordRegex, {message: 'The password must contain uppercase letters, lowercase letters, and numbers.' })
   @IsStrongPassword({minLength: 8}, {message: "The password must be at least 8 characters long" })
   password !: string;
 
   @IsString()
-  @MinLength(2)
-  @MaxLength(50)
+  @MinLength(2, {message: 'The name must contain at least 2 characters'})
+  @MaxLength(50, {message: 'The name is too long'})
   name !: string;
 }
