278fe8b04b9e42847592998db8857a8dc95e3a0e
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-09-08 18:12:07 -0300

    feat: remove unused imports from auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,4 @@
 import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { UsersService } from 'src/users/users.service';
 import { JwtService, JwtSignOptions } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config'
