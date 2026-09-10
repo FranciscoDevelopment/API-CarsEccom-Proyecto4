0fdf88d3c401ea2c4e678cad26488cb6a8e30b50
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-09-10 16:32:42 -0300

    feat: add authentication to create, update, and delete version endpoints in VersionsController

--- src/versions/versions.controller.ts
+++ src/versions/versions.controller.ts
@@ -3,17 +3,21 @@ import { VersionsService } from './versions.service';
 import { CreateVersionDto } from './dto/create-version.dto';
 import { UpdateVersionDto } from './dto/update-version.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { RoleEnum } from 'src/auth/types/role.type';
 
 @Controller({path: 'versions', version: '1'})
 export class VersionsController {
+
   constructor(private readonly versionsService: VersionsService) {}
 
+  
+  @Auth( RoleEnum.ADMIN )
   @Post()
   create(@Body() createVersionDto: CreateVersionDto) {
     return this.versionsService.create(createVersionDto);
   }
 
-  
   
   @Get()
   async findWithPagination( @Query() pagination : PaginationQueryDto ) {
@@ -21,6 +25,7 @@ export class VersionsController {
     return await this.versionsService.findWithPagination( pagination.page, pagination.pageSize )
     
   }
+  
   
   @Get('all')
   findAll() {
@@ -57,13 +62,19 @@ export class VersionsController {
     return await this.versionsService.findOne(+id);
   }
 
+
+  @Auth( RoleEnum.ADMIN )
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateVersionDto: UpdateVersionDto) {
     return await this.versionsService.update(+id, updateVersionDto);
   }
 
+
+  @Auth( RoleEnum.ADMIN )
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.versionsService.remove(+id);
   }
+
+
 }
